1a47f37c43006748bc0e5f4d5c05b72f9c85a223
Hoon <hoonahn86@gmail.com>
2025-01-17 12:14:12 +0900

    Update Streamlit app to include environment key

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
 import streamlit as st
 
 st.title('Deploy App')
+st.write('My First Deploy App !')
 
-st.write('My first Deploy App !')
+import os
+key = os.environ.get('MT_SECRET', 'NOT SET YET')
+st.write(f'Server Key : {key}')
+
